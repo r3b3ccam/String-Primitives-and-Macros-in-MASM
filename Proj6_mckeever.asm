d1c76157ba254cb7283f0dfd7f2b9e2596385c09
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-03-07 22:43:00 -0600

    initial commit

---
+++ Proj6_mckeever.asm
@@ -0,0 +1,30 @@
+TITLE Project 6 - String Primitives and Macros     (Proj6_mckeever.asm)
+
+; Author: Rebecca Mckeever
+; Last Modified: 03/07/2021
+; OSU email address: ONID_ID@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 6                Due Date: 03/16/2021
+; Description: ***
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
